c0b2ad06bcf1c60b23fe95c64e4ceb17015e7995
DhavitG <gdhavit@gmail.com>
2025-04-08 23:36:29 +0530

    Intro to props

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div style={{ backgroundColor: "#dfe6e9", height: "100vh" }}>
-      <PostComponent />
+    <div
+      style={{
+        backgroundColor: "#dfe6e9",
+        height: "100vh",
+      }}
+    >
+      <ToggleMessage />
+      <Greeting name={"100xdhavit"} />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>
+          <div>
+            <PostComponent
+              name={"100xdhavit"}
+              subtitle={"20,000 followers"}
+              time={12}
+              profilePhoto={
+                "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
+              }
+              description={
+                "Want to know how to win big? Check out these folks who got $6000 bounties on the regular."
+              }
+            />
+          </div>
+          <br />
+          <div>
+            <PostComponent
+              name={"Linkedin Marketing"}
+              subtitle={"Promoted"}
+              profilePhoto={
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRokEYt0yyh6uNDKL8uksVLlhZ35laKNQgZ9g&s"
+              }
+              description={
+                "Want to know how to win big? Check out these folks who got $6000 bounties on the regular."
+              }
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
@@ -14,16 +51,41 @@ const style = {
   borderRadius: 10,
   borderColor: "gray",
   borderWidth: 1,
+  padding: 30,
 };
 
-function PostComponent() {
+const ToggleMessage = () => {
+  const [isVisible, setIsVisible] = useState(false); // defining a new state
+  // when the value of this state changes
+  // ToggleMessage component re-renders
+
+  return (
+    <div>
+      <button
+        onClick={() => setIsVisible(!isVisible)}
+        style={{ marginTop: 15 }}
+      >
+        Toggle Message
+      </button>
+      {isVisible && <p>This message is conditionally rendered.</p>}
+    </div>
+  );
+};
+
+function Greeting({ name }) {
+  return (
+    <div style={{ fontSize: 38 }}>
+      <b>Hello {name}!</b>
+    </div>
+  );
+}
+
+function PostComponent({ name, subtitle, time, profilePhoto, description }) {
   return (
     <div style={style}>
       <div style={{ display: "flex" }}>
         <img
-          src={
-            "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
-          }
+          src={profilePhoto}
           style={{
             width: 30,
             height: 30,
@@ -31,16 +93,23 @@ function PostComponent() {
           }}
         />
         <div style={{ fontSize: 14, marginLeft: 10 }}>
-          <b>100xdhavit</b>
-          <div>23,000 followers</div>
-          <div>12m</div>
+          <b>{name}</b>
+          <div>{subtitle}</div>
+          {time && (
+            <div style={{ display: "flex" }}>
+              <div>{time}m &#8226;</div>
+              <img
+                src={
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5PpNh5nNkcB83Sfwp-V3NbdJ6bku2T6Kk_g&s"
+                }
+                style={{ width: 12, height: 12, marginLeft: 2 }}
+              />
+            </div>
+          )}
         </div>
       </div>
 
-      <div style={{ fontSize: 16 }}>
-        Want to know how to win big? Check out these folks who got $6000
-        bounties on the regular.
-      </div>
+      <div style={{ fontSize: 16 }}>{description}</div>
     </div>
   );
 }
