707eb7f6f3908ce1a4251032e354cd9aa2125dfe
DhavitG <gdhavit@gmail.com>
2025-06-17 04:26:43 +0530

    basic assignment

--- week-13-tailwind/src/App.jsx
+++ week-13-tailwind/src/App.jsx
@@ -1,9 +1,21 @@
+// function App() {
+//   return (
+//     <div className="grid grid-cols-12">
+//       <div className="bg-blue-300 col-span-4">child1</div>
+//       <div className="bg-green-300 col-span-6">child2</div>
+//       <div className="bg-purple-300 col-span-2">child3</div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 function App() {
   return (
-    <div className="grid grid-cols-12">
-      <div className="bg-blue-300 col-span-4">child1</div>
-      <div className="bg-green-300 col-span-6">child2</div>
-      <div className="bg-purple-300 col-span-2">child3</div>
+    <div className="grid grid-cols-1 sm:grid-cols-12">
+      <div className="bg-blue-300 col-span-1 sm:col-span-5">child1</div>
+      <div className="bg-green-300 col-span-1 sm:col-span-5">child2</div>
+      <div className="bg-purple-300 col-span-1 sm:col-span-2">child3</div>
     </div>
   );
 }
