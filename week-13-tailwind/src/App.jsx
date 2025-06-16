d49361dfc113d87e402f8e4ad8e288f86bc63a5f
DhavitG <gdhavit@gmail.com>
2025-06-17 03:59:16 +0530

    flex and grids

--- week-13-tailwind/src/App.jsx
+++ week-13-tailwind/src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div className=" text-yellow-300 bg-black">hi</div>;
+  return (
+    <div className="grid grid-cols-12">
+      <div className="bg-blue-300 col-span-4">child1</div>
+      <div className="bg-green-300 col-span-6">child2</div>
+      <div className="bg-purple-300 col-span-2">child3</div>
+    </div>
+  );
 }
 
 export default App;
